6d98d8272cf794925c51459deb2fb1cf94a35172
Xand Meaden <xand@xand.uk>
2020-05-07 13:06:44 +0100

    update for 19.2.7

--- winbareos.install/tools/chocolateyInstall.ps1
+++ winbareos.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,20 @@
 $packageName = 'winbareos.install'
-$installerType = 'exe'
-$url = 'http://download.bareos.org/bareos/release/14.2/windows/winbareos-14.2.2.postvista-32-bit-r16.1.exe'
-$url64 = 'http://download.bareos.org/bareos/release/14.2/windows/winbareos-14.2.2.postvista-64-bit-r16.1.exe'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+$url = 'https://download.bareos.org/bareos/release/latest/windows/winbareos-19.2.7-release-32-bit.exe'
+$url64 = 'https://download.bareos.org/bareos/release/latest/windows/winbareos-19.2.7-release-64-bit.exe'
+$checksum = '3ca9fe7bc67dc042f9f97ab448f05a733502833bfcade3b26ccef900545e7faf'
+$checksum64 = 'd2880a6110c7a1a42aed7bb97f0bcb176f58427223eff1c305a183382cfc1055'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName = $packageName
+  fileType = 'exe'
+  url = $url
+  url64 = $url64
+  silentArgs = '/S'
+  validExitCodes = @(0)
+  checksum = $checksum
+  checksumType = 'sha256'
+  checksum64 = $checksum64
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
